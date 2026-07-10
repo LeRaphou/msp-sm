f6d3aa610c62368feecb336a1b38f48485ff7dae
LeRaphou <raphybonnet2006@gmail.com>
2026-07-10 10:40:52 +0200

    Refactor Sidebar component to use Link elements for navigation; add new navLinks file for better structure

---
+++ app/ui/navLinks.ts
@@ -0,0 +1,8 @@
+export const navLinks = [
+  { label: "Accueil", href: "/" },
+  { label: "Présentation", href: "/presentation" },
+  { label: "Contact", href: "/contact" },
+  {label: "stock", href: "/stock"},
+  {label: "planning", href: "/planning"},
+  {label: "professionnels", href: "/professionnels"},  
+];
--- app/ui/sidebar.tsx
+++ app/ui/sidebar.tsx
@@ -1,19 +1,38 @@
-const navItems = ["Accueil", "Présentation", "Contact"];
+import Link from "next/link";
 
 export default function Sidebar() {
   return (
-    <aside className="hidden min-h-[70vh] w-60 rounded-2xl border border-slate-200 bg-slate-900 p-4 text-white shadow-sm md:flex md:flex-col md:gap-2 items-center">
-      <div className="mb-2 px-2 py-1 text-sm font-semibold uppercase tracking-[0.2em] text-slate-400">
+    <aside className="hidden min-h-full w-60 rounded-3xl border border-slate-200 bg-slate-900 p-4 text-white shadow-sm md:flex md:flex-col md:gap-2 items-center justify-start">
+      <div className="mb-2 px-2 py-1 text-sm font-semibold uppercase tracking-[0.2em] text-slate-400 ">
         Navigation
       </div>
-      {navItems.map((item) => (
-        <button
-          key={item}
-          className="rounded-xl px-3 py-3 text-left text-sm font-medium text-slate-200 transition hover:bg-slate-800"
-        >
-          {item}
-        </button>
-      ))}
+
+
+      <div className="flex flex-1 flex-col gap-2 items-center ">
+        <Link href="/accueil">
+  <button>Accueil</button>
+</Link>
+
+<Link href="/presentation">
+  <button>Présentation</button>
+</Link>
+
+<Link href="/contact">
+  <button>Contact</button>
+</Link>
+
+<Link href="/stock">
+  <button>Stock</button>
+</Link>
+
+<Link href="/planning">
+  <button>Planning</button>
+</Link>
+
+<Link href="/professionnels">
+  <button>Professionnels</button>
+</Link>
+      </div>
     </aside>
   );
 }
