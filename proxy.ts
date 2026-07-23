d3cd76c0c2ff10e2dfa90ed19beea655eb0dea90
LeRaphou <raphybonnet2006@gmail.com>
2026-07-23 11:18:19 +0200

    removed auth scripts

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth"
-import { authConfig } from "./auth.config"
+import { authConfig } from "./app/api/auth/auth.config"
 
 export default NextAuth(authConfig).auth
 
 export const config = {
-  matcher: ["/dashboard/:path*" , "/register/:path*", "/login/:path*"],
+  matcher: ["/dashboard/:path*" ],
 }
